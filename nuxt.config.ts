d9307d6a414febf498e1e3532e0d413401549044
Ennio Visconti <ennio.visconti@gmail.com>
2023-04-13 14:05:59 +0200

    added swr and reduced image sizes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/alpine',
   css: ["@/assets/main.css"],
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      htmlAttrs: {
+        lang: 'en',
+      },
+    }
+  },
+
+  routeRules: {
+    '/**': {swr: true},
+  }
 })
